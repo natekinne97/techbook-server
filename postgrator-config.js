858ec420cad1aab0c0300e30957a0aca1c961618
Nate Kinne <nathan.kinne@gmail.com>
2019-11-05 22:15:51 -0600

    deploy mod

--- postgrator-config.js
+++ postgrator-config.js
@@ -7,6 +7,7 @@ require('dotenv').config()
 const { expect } = require('chai')
 const supertest = require('supertest')
 
+// for running on local
 module.exports = {
     "migrationDirectory": "migrations",
     "driver": "pg",
@@ -14,5 +15,18 @@ module.exports = {
     "port": process.env.MIGRATION_DB_PORT,
     "database": process.env.MIGRATION_DB_NAME,
     "username": process.env.MIGRATION_DB_USER,
-    "password": process.env.MIGRATION_DB_PASS
+    "password": process.env.MIGRATION_DB_PASS,
+    "connectionString": (process.env.NODE_ENV === 'test')
+        ? process.env.TEST_DATABASE_URL
+        : process.env.DATABASE_URL,
+    "ssl": !!process.env.SSL,
 }
+
+// module.exports = {
+//     "migrationDirectory": "migrations",
+//     "driver": "pg",
+//     "connectionString": (process.env.NODE_ENV === 'test')
+//         ? process.env.TEST_DATABASE_URL
+//         : process.env.DATABASE_URL,
+//         "ssl": !!process.env.SSL,
+//   }
